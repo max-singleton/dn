50653500bfe1a37796cfb49a87d473f8af5ddbed
max-singleton <phpcrud@mail.ru>
2019-08-05 15:19:29 +0300

    Добавил кнопку скачать

--- docs.php
+++ docs.php
@@ -14,295 +14,154 @@
     </div>
 </div>
 
-    <!-- Контент. Начало -->
-    <div class="container bg-white">
-        <div class="row o-company">
-            <div class="col-md-4">
+<!-- Контент. Начало -->
+<div class="container bg-white">
+    <div class="row o-company">
+        <div class="col-md-4">
 
-                <?php include 'about-company-submenu.php' ?>
-
-            </div>
-
-            <div class="col-md-8 col-sm-12 col-xs-12">
-                <!--work_area. Начало -->
-
-                <div class="">
-                    <center><h3>
-                            Тарифы на коммунальные услуги на 2019 год
-                        </h3></center>
-                </div>
-
-                <style>
-                    td, tr {
-                        border: 2px solid #fff;
-                        text-align: center;
-                    }
-
-                    .velosiped {
-                        overflow-x: scroll;
-                    }
-                </style>
-
-                <div class="velosiped">
-                    <table class="table table-hover alert-info">
-                        <thead class="table-info alert-primary">
-                        <tr>
-                            <th style="border: 2px solid #007099" scope="col">
-                                #<br><br><br>
-                            </th>
-                            <th style="border: 2px solid #007099" scope="col">
-                                Наименование услуг<br><br><br>
-                            </th>
-                            <th style="border: 2px solid #007099" scope="col">
-                                Единица измерения<br><br>
-                            </th>
-                            <th style="border: 2px solid #007099" scope="col">
-                                Размер платы, тариф на единицу услуг (руб. коп.) с 01.01.2019
-                            </th>
-                            <th style="border: 2px solid #007099" scope="col">
-                                Размер платы, тариф на единицу услуг (руб. коп.) с 01.07.2019
-                            </th>
-                            <th style="border: 2px solid #007099" scope="col">
-                                Нормативно-правовой документ<br><br>
-                            </th>
-                        </tr>
-                        </thead>
-                        <tbody>
-
-
-                        <tr>
-                            <td>
-                                <b>1</b>
-                            </td>
-                            <td>
-                                Тепловая энергия, используемая на отопление
-                            </td>
-                            <td>
-                                руб./Гкал
-                            </td>
-                            <td>
-                                1548,77
-                            </td>
-                            <td>
-                                1556,02
-                            </td>
-                            <td>
-                                Постановление Государственной службы ЧР по конкурентной политике и тарифам от 14 декабря
-                                2018 г. № 134-32/т.
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td>
-                                <b>2</b>
-                            </td>
-                            <td>
-                                Компонент на теплоноситель ГВС
-                            </td>
-                            <td>
-                                руб./куб. м.
-                            </td>
-                            <td>
-                                35,90
-                            </td>
-                            <td>
-                                36,58
-                            </td>
-                            <td rowspan="2">
-                                Постановление Государственной службы ЧР по конкурентной политике и тарифам от 14 декабря
-                                2018 г. № 135-32/гв.
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td>
-                                <b>3</b>
-                            </td>
-                            <td>
-                                Компонент на тепловую энергию ГВС
-                            </td>
-                            <td>
-                                руб./Гкал
-                            </td>
-                            <td>
-                                1548,77
-                            </td>
-                            <td>
-                                1556,02
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td>
-                                <b>4</b>
-                            </td>
-                            <td>
-                                Холодное водоснабжение
-                            </td>
-                            <td>
-                                руб./куб. м.
-                            </td>
-                            <td>
-                                18,08
-                            </td>
-                            <td>
-                                18,46
-                            </td>
-                            <td rowspan="2">
-                                Постановление Государственной службы ЧР по конкурентной политике и тарифам от 06 декабря
-                                2018 г. № 83-30/в.
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td>
-                                <b>5</b>
-                            </td>
-                            <td>
-                                Водоотведение
-                            </td>
-                            <td>
-                                Руб./Гкал
-                            </td>
-                            <td>
-                                20,98
-                            </td>
-                            <td>
-                                21,37
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td>
-                                <b>6</b>
-                            </td>
-                            <td>
-                                Электроэнергия в жилых домах, оборудованных газовыми плитами
-                            </td>
-                            <td>
-                                Руб./кВт в час
-                            </td>
-                            <td>
-                                3,30
-                            </td>
-                            <td>
-                                3,36
-                            </td>
-                            <td rowspan="4">
-                                Постановление Государственной службы ЧР по конкурентной политике и тарифам от12 декабря 2018
-                                г. № 128-32/э
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td>
-                                <b>7</b>
-                            </td>
-                            <td>
-                                Электроэнергия в жилых домах, оборудованных стационарными электро-плитами
-                            </td>
-                            <td>
-                                Руб/кВт в час
-                            </td>
-                            <td>
-                                2,31
-                            </td>
-                            <td>
-                                2,35
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td>
-                                <b>8</b>
-                            </td>
-                            <td>
-                                Тариф за электроэнергию в жилых домах, оборудованных газовыми плитами, дифференцированный по
-                                зонам суток: дневная зона / ночная зона
-                            </td>
-                            <td>
-                                Руб/кВт в час
-                            </td>
-                            <td>
-                                3,80 / 2,15
-                            </td>
-                            <td>
-                                3,86 / 2,19
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td>
-                                <b>9</b>
-                            </td>
-                            <td>
-                                Тариф за электроэнергию в жилых домах, оборудованных стационарными электроплитами,
-                                дифференцированный по зонам суток: дневная зона / ночная зона
-                            </td>
-                            <td>
-                                руб./кВт в час
-                            </td>
-                            <td>
-                                2,66 / 1,49
-                            </td>
-                            <td>
-                                2,70 / 1,52
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td>
-                                <b>10</b>
-                            </td>
-                            <td>
-                                Тариф по обращению с твёрдыми коммунальными отходами
-                            </td>
-                            <td>
-                                руб./куб. м.
-                            </td>
-                            <td>
-                                449,71
-                            </td>
-                            <td>
-                                456,82
-                            </td>
-                            <td>
-                                Постановление Государственной службы ЧР по конкурентной политике и тарифам от 06 декабря
-                                2018 г. № 109-30/в.
-                            </td>
-                        </tr>
-
-                        </tbody>
-                    </table>
-                </div>
-
-                <!--work_area. Конец -->
-            </div>
+            <?php include 'about-company-submenu.php' ?>
 
         </div>
-    </div>
 
-    <!-- Контент. Конец -->
-	
-    <!-- Включение входа в личный кабинет -->
-	<?php include './template/login.php'; ?>
-    
-	<!-- Включение футера -->
-    <?php include './template/footer.php'; ?>
-	
-	<!-- Включение мобильного меню -->
-	<?php include './template/mobile_menu.php'; ?>
-    
-    <!-- Скрипты. jQuery первый, затем Tether, затем Bootstrap JS. -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"
+        <div class="col-md-8 col-sm-12 col-xs-12">
+            <!--work_area. Начало -->
+            <style>
+                @import url('https://fonts.googleapis.com/css?family=Mukta:700');
+                * {
+                    box-sizing: border-box;
+                }
+                *::before, *::after {
+                    box-sizing: border-box;
+                }
+                button {
+                    position: relative;
+                    display: inline-block;
+                    cursor: pointer;
+                    outline: none;
+                    border: 0;
+                    vertical-align: middle;
+                    text-decoration: none;
+                    background: transparent;
+                    padding: 0;
+                    font-size: inherit;
+                    font-family: inherit;
+                }
+                button.learn-more {
+                    width: 12rem;
+                    height: auto;
+                }
+                button.learn-more .circle {
+                    transition: all 0.45s cubic-bezier(0.65, 0, 0.076, 1);
+                    position: relative;
+                    margin: 0;
+                    width: 3rem;
+                    height: 3rem;
+                    background: #4a76a8;
+                    border-radius: 1.625rem;
+                }
+                button.learn-more .circle .icon {
+                    transition: all 0.45s cubic-bezier(0.65, 0, 0.076, 1);
+                    position: absolute;
+                    top: 0;
+                    bottom: 0;
+                    margin: auto;
+                    background: #fff;
+                }
+                button.learn-more .circle .icon.arrow {
+                    transition: all 0.45s cubic-bezier(0.65, 0, 0.076, 1);
+                    left: 0.625rem;
+                    width: 1.125rem;
+                    height: 0.125rem;
+                    background: none;
+                }
+                button.learn-more .circle .icon.arrow::before {
+                    position: absolute;
+                    content: '';
+                    top: -0.25rem;
+                    right: 0.0625rem;
+                    width: 0.625rem;
+                    height: 0.625rem;
+                    border-top: 0.125rem solid #fff;
+                    border-right: 0.125rem solid #fff;
+                    transform: rotate(45deg);
+                }
+                button.learn-more .button-text {
+                    transition: all 0.45s cubic-bezier(0.65, 0, 0.076, 1);
+                    position: absolute;
+                    top: 0;
+                    left: 0;
+                    right: 0;
+                    bottom: 0;
+                    padding: 0.75rem 0;
+                    margin: 0 0 0 1.85rem;
+                    color: #4a76a8;
+                    font-weight: 700;
+                    line-height: 1.6;
+                    text-align: center;
+                    /*text-transform: uppercase;*/
+                }
+                button:hover .circle {
+                    width: 100%;
+                }
+                button:hover .circle .icon.arrow {
+                    background: #fff;
+                    transform: translate(1rem, 0);
+                }
+                button:hover .button-text {
+                    color: #fff;
+                }
+                @supports (display: grid) {
+                    #container {
+                        grid-area: main;
+                        align-self: center;
+                        justify-self: center;
+                    }
+                }
+
+            </style>
+
+
+            <div id="container">
+                <span style="font-size:18px;font-weight:bold;">&#8226; Тарифы на коммунальные услуги на 2019 год &nbsp;</span>
+                <button class="learn-more">
+                    <div class="circle">
+                        <span class="icon arrow"></span>
+                    </div>
+
+                    <a href="uplouds/tarif2019.doc" download><p class="button-text">Скачать</p></a>
+                </button>
+            </div>
+
+            <!--Надо флексбокс применить потом-->
+            <br><br><br><br><br><br><br><br><br><br><br>
+
+            <!--work_area. Конец -->
+        </div>
+
+    </div>
+</div>
+
+<!-- Контент. Конец -->
+
+<!-- Включение входа в личный кабинет -->
+<?php include './template/login.php'; ?>
+
+<!-- Включение футера -->
+<?php include './template/footer.php'; ?>
+
+<!-- Включение мобильного меню -->
+<?php include './template/mobile_menu.php'; ?>
+
+<!-- Скрипты. jQuery первый, затем Tether, затем Bootstrap JS. -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"
         integrity="sha384-THPy051/pYDQGanwU6poAc/hOdQxjnOEXzbT+OuUAFqNqFjL+4IGLBgCJC3ZOShY"
         crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.2.0/js/tether.min.js"
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.2.0/js/tether.min.js"
         integrity="sha384-Plbmg8JY28KFelvJVai01l8WyZzrYWG825m+cZ0eDDS1f7d/js6ikvy1+X+guPIB"
         crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.4/js/bootstrap.min.js"
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.4/js/bootstrap.min.js"
         integrity="VjEeINv9OSwtWFLAtmc4JCtEJXXBub00gtSnszmspDLCtC0I4z4nqz7rEFbIZLLU" crossorigin="anonymous"></script>
-    <script src="js/script.js"></script>
+<script src="js/script.js"></script>
 
 </body>
 
